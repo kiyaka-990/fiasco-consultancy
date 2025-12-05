e9f1f906e9dcb1ec4f5014147494e858cedc4f98
kiyaka-990 <kiyakaokullu@gmail.com>
2025-12-05 06:44:28 +0300

    updated build

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Other configurations can go here, but output: 'export' is GONE
-};
-
-export default nextConfig; // or export default nextConfig;
---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Other configurations can go here
+};
+
+// Node.js expects ESM syntax for 'next.config.mjs'
+export default nextConfig;
